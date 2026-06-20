c104a07722dfff2ac91f5ee9e6426fe632619fdf
slaranenianroy-dot <slaranenianroy@gmail.com>
2026-06-20 16:59:54 +0530

    Create project.java

---
+++ project.java
@@ -0,0 +1,142 @@
+package first;
+import java.util.*;
+public class project {
+/*static int power(int x,int pow) {
+		int result=1;
+		while(pow>0) {
+			result*=x;
+			pow--;
+		}
+		return result;
+	}*/
+	
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		/*int n=sc.
+		 * int sumodd=0;
+		int sumeven=0;
+		for (int i=0;i<=10;i++) {
+			
+			if (i%2==0) {
+				sumeven+=i;
+			}
+			else {
+				sumodd+=i;
+			}
+		}
+		System.out.println(sumodd);
+		System.out.println(sumeven);*/
+		
+		
+		/*System.out.println("Enter the number: ");
+		int n=sc.nextInt();
+		for (int i=0;i<10;i++) {
+			System.out.println(n+"x"+i+"="+n*i);
+		}*/
+		
+		
+		/*System.out.println("Enter the number: ");
+		int n=sc.nextInt();
+		int fact=1;
+		for (int i=n;i>=1;i--) {
+			fact*=i;
+		}
+		System.out.println(fact);*/
+		
+		/*for(char i='a';i<='z';i++) {
+			System.out.println(i);
+		}*/
+		
+		/*int m=5,n=10;
+		for (int i=n;i>=1;i--) {
+			System.out.println(m*i);
+		}*/
+		
+		/*System.out.println("Enter the number: ");
+		int n=sc.nextInt();
+		int sum=0;
+		while(n>0) {
+			sum+=n%10;
+			n=n/10;
+		}
+		if(n%sum==0) {
+			System.out.println("Harshad Number");
+		}
+		else {
+			System.out.println("Not a Harshad Number");
+		}*/
+		
+		/*System.out.println("Enter the number: ");
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++) {
+			if(n%i==0) {
+				sum+=i;
+			}
+		}
+		System.out.println(sum);*/
+		
+		/*System.out.println("Enter the number: ");
+		int n=sc.nextInt();
+		int num=0;
+		int temp=n;
+		while(n>0) {
+			num=10*num+n%10;
+			n=n/10;
+		}
+		if(temp==num) {
+			System.out.println("Palindrome");
+		}
+		else {
+			System.out.println("Not a Palindrome");
+		}*/
+		
+		/*System.out.println("Enter the number: ");
+		int n=sc.nextInt();
+		int pow=0;
+		int temp=n;
+		int temp2=n;
+		while(n>0) {
+			n=n/10;
+			pow++;
+		}
+		int res=0;
+		while(temp>0) {
+			int x=temp%10;
+			res+=power(x,pow);
+			temp=temp/10;
+		}
+		if (res==temp2) {
+			System.out.println("Armstrong Number");
+		}
+		else {
+			System.out.println("Not a Armstrong Number");
+		}*/
+		
+		/*int a[]=new int[20];
+		System.out.println("Enter the value of the array: ");
+		for(int i=0;i<20;i++) {
+			a[i]=sc.nextInt();
+		}
+		int max=a[0], min=a[0], sum=0;
+		for(int i=1;i<20;i++) {
+			if(a[i]>max) {
+				max=a[i];
+			}
+		}
+		for(int i=1;i<20;i++) {
+			if(a[i]<min) {
+				min=a[i];
+			}
+		}
+		for(int i=0;i<20;i++) {
+			sum+=a[i];
+		}
+		System.out.println("Minimum: "+min);
+		System.out.println("Maximum: "+max);
+		System.out.println("Sum: "+sum);*/
+  }
+}
+		
+		
